38fec62bf4a572663ff34bf28a602faa19352160
Yahid1 <yahid.diaz@upr.edu>
2025-05-27 16:12:18 -0400

    We need a fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,30 @@
 import { Text, View } from "react-native";
+import { BLEService } from './BLE';
+import { useEffect } from "react";
 
 export default function Index() {
+  function scanAndConnect() {
+  BLEService.manager.startDeviceScan(null, null, (error:any, device:any) => {
+    if (error) {
+      // Handle error (scanning will be stopped automatically)
+      return
+    }
+
+    // Check if it is a device you are looking for based on advertisement data
+    // or other criteria.
+    if (device.name === 'TI BLE Sensor Tag' || device.name === 'SensorTag') {
+      // Stop scanning as it's not necessary if you are scanning for one device.
+      BLEService.manager.stopDeviceScan()
+
+      // Proceed with connection.
+    }
+  })
+}
+  scanAndConnect();
+  useEffect(() => {}, []);
+  // Initialize BLE manager
+  
+
   return (
     <View
       style={{
@@ -9,6 +33,14 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold",
+          color: "#333",
+        }}>
+          Hello, World!
+        </Text>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
